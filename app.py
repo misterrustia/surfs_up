c1244798bb61d60fb970617b3a8b1ab4f3e7178c
Robert Rustia <analyticalmr.r@gmail.com>
2020-07-09 14:34:14 -0700

    9.5.5 complete

--- app.py
+++ app.py
@@ -1,8 +1,97 @@
-from flask import Flask
+# import dependencys
+import numpy as np
+import datetime as dt 
+import pandas as pd 
 
 
-app = Flask(__name__)
+# import SQLAlchemy dependencies 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-@app.route('/')
-def hello_world():
-    return 'Hello world ,whoot whoot first webpage made'
+# import flask depends
+from flask import Flask, jsonify 
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+#create variable for each class so that we can reference them later 
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# create a session link from python to our database with the following code 
+session = Session(engine)
+
+# create flask application called "app"
+app=Flask(__name__)
+# all routes go after this 
+
+# name will be set to what ever is running the code, if we put this in something else the name will change to 
+#to what ever is running this, if we run directly with python app.py the name will be "main"
+
+#define welcome route 
+@app.route("/")
+
+# create fnc 
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
+        
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017,8,23)-dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date:prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations = stations)
+
+
+# prev year is object with a year of days from 8,23 and is the filter for the measurement 
+# measurement data being queried from session in SQLA 
+
+
+# Jsonify in a fnc to convert dictionarys to JSON files 
+
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017,8,23)-dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.tobs).\
+    filter(Measurement.station =='USC00519281').\
+    filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+        
+    return jsonify(temps=temps)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0',port = 5000)
+
+
+#cal date 1 yr from last date in DB
+#query primary stayion for all temp obs from prvs year
+#numpy ravel into 1 d array then convert into list
+#temps = a list of the 1 d aray numpy usese ravel to create from the results 
